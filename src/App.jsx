fe30bf7f8ca2ec0cb9a8873aa15baaedb49517aa
Salman Afridi <salmanafridi.dev@gmail.com>
2026-07-11 22:13:36 +0500

    New components added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,39 @@
-import React from "react";
+import { useState, useEffect } from "react";
+import { useDispatch } from "react-redux";
+import authService from "./appwrite/auth.js";
+import { login, logout } from "./store/authSlice.js";
+import Header from "./components/Header/Header.jsx";
+import Footer from "./components/Footer/Footer.jsx";
+import { Outlet } from "react-router-dom";
 
 function App() {
-  return (
-    <div>
-      <h2>App write application</h2>
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    authService
+      .getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login(userData));
+        } else {
+          dispatch(logout());
+        }
+      })
+      .finally(() => setLoading(false));
+  }, [dispatch]);
+
+  return !loading ? (
+    <div className="min-h-screen flex flex-wrap content-between bg-gray-400 ">
+      <div className="w-full block">
+        <Header />
+        <main>
+          <Outlet />
+        </main>
+        <Footer />
+      </div>
     </div>
-  );
+  ) : null;
 }
 
 export default App;
