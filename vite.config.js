278684ee4f6074b42d80f0ab695de9a7c761dd54
Salman Afridi <salmanafridi.dev@gmail.com>
2026-07-15 21:52:27 +0500

    Config file updated

--- vite.config.js
+++ vite.config.js
@@ -7,16 +7,5 @@ export default defineConfig({
   build: {
     // Raise the warning threshold to 800 KB
     chunkSizeWarningLimit: 800,
-    rollupOptions: {
-      output: {
-        // Split large vendor libraries into separate chunks
-        manualChunks: {
-          "vendor-react":   ["react", "react-dom", "react-router-dom"],
-          "vendor-redux":   ["@reduxjs/toolkit", "react-redux"],
-          "vendor-appwrite": ["appwrite"],
-          "vendor-tinymce": ["@tinymce/tinymce-react"],
-        },
-      },
-    },
   },
 });
